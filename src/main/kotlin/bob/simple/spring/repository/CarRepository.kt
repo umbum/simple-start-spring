464b95fb22b438adb273ea4f0baeed9d59df2b8f
umbum <umbum7601@gmail.com>
2024-07-20 15:33:45 +0900

    sql param, result mapping을 직접

--- src/main/kotlin/bob/simple/spring/repository/CarRepository.kt
+++ src/main/kotlin/bob/simple/spring/repository/CarRepository.kt
@@ -3,7 +3,9 @@ package bob.simple.spring.repository
 import bob.simple.spring.model.Car
 import org.springframework.jdbc.core.BeanPropertyRowMapper
 import org.springframework.jdbc.core.DataClassRowMapper
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
 import org.springframework.stereotype.Repository
 
@@ -15,15 +17,23 @@ class CarRepository(
     fun selectBy(id: String): Car {
         return jdbcOperations.queryForObject(
             "select * from car where id = :id",
-            mapOf("id" to id),
-            DataClassRowMapper(Car::class.java),
-        )!!
+            mapOf("id" to id)
+        ) { rs, _ ->
+            Car(
+                id = rs.getString("id"),
+                acStatus = rs.getBoolean("ac_status"),
+                model = rs.getString("model"),
+            )
+        }!!
     }
 
     fun update(car: Car) {
         jdbcOperations.update(
             "update car set ac_status = :acStatus, model = :model where id = :id",
-            BeanPropertySqlParameterSource(car)
+            MapSqlParameterSource()
+                .addValue("acStatus", car.acStatus)
+                .addValue("model", car.model)
+                .addValue("id", car.id)
         )
     }
 }
